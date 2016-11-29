0afbd806aed787104fe8e3327a843768d33dbe43
SancheCarlos <sanche_c@etna-alternance.net>
2016-11-29 11:29:39 +0100

    Add files via upload

--- formulaire_json.js
+++ formulaire_json.js
@@ -1,4 +1,18 @@
 var testJSON;
-testJSON = {"code_postal":"95170"};
-document.getElementbyID('test').innerHTML = testJSON.code_postal;
+testJSON = {"code_postal" : "95170"};
+document.getElementById('test').innerHTML = testJSON.code_postal;
 //$("#codepostal").attr("placeholder", testJSON.code_postal);
+	var prop = {
+			"title": "contact", // the name of our JSON
+			"type": "object", // we're declaring an object
+			"properties": {
+				"myname" : "carlos",
+				/* properties we want: postcode...
+				** we must precise the type of the data */
+				"postcode":{	
+					"type": "string"
+				}
+			}
+	}
+
+	document.getElementById('test').innerHTML = prop.title;
